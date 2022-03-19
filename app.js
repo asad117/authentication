9d185d0ef512e07314cff505c0abf6278300d9f1
Asad Hashmi <asad4hashmi@gmail.com>
2022-03-19 18:05:23 +0530

    hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
+
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -13,11 +14,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
-const userSchema = new mongoose.Schema({
+const userSchema =({
   email :String,
   password : String,
 });
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 const User = mongoose.model("User", userSchema)
 
 app.use(express.static(__dirname + "/public"));
@@ -37,7 +38,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -52,7 +53,7 @@ app.post("/register", function(req,res){
 ////////////////////////For exixting User Login /////////////////////////
 app.post("/login", function(req,res){
   const useremail = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   User.findOne({email:useremail}, function(err, foundUser){
     if(err){
       console.log(err);
